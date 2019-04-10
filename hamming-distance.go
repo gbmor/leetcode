354e8c0a35a23ce31e822af8b59c4804826ebd63
Ben Morrison <ben@gbmor.dev>
2019-04-10 01:27:38 -0400

    hamming distance of integers

---
+++ hamming-distance.go
@@ -0,0 +1,13 @@
+// Package leetcode :: leetcode.com
+// Ben Morrison
+// ben@gbmor.dev
+package leetcode
+
+// hammingDistance() returns the 'hamming distance'
+// of two integers.
+// Leetcode.com stats: 0ms, 2MB memory.
+// Ahead of 100% of submissions.
+func hammingDistance(x int, y int) int {
+	    return bits.OnesCount(uint(x) ^ uint(y))
+
+}
