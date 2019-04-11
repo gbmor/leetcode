efde7593ebc0770a09084e9dc3bd65eb4678d524
Ben Morrison <ben@gbmor.dev>
2019-04-11 01:07:06 -0400

    fixed imports

--- hamming-distance.go
+++ hamming-distance.go
@@ -3,11 +3,15 @@
 // ben@gbmor.dev
 package leetcode
 
+import (
+	"math/bits"
+)
+
 // hammingDistance() returns the 'hamming distance'
 // of two integers.
 // Leetcode.com stats: 0ms, 2MB memory.
 // Ahead of 100% of submissions.
 func hammingDistance(x int, y int) int {
-	    return bits.OnesCount(uint(x) ^ uint(y))
+	return bits.OnesCount(uint(x) ^ uint(y))
 
 }
